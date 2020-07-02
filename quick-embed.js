d7148921f3b3239eb41768bf114cf6b711571bfc
Connor Bond <connorbond@outlook.com>
2020-07-02 14:36:04 +0100

    Add loading state styles, strengthen ping interval validation

--- quick-embed.js
+++ quick-embed.js
@@ -3,6 +3,39 @@
  */
 (async function () {
 
+    // Inject loading styles
+    addHeaderStyle(`
+        [data-dw-form] {
+            position: relative;
+            margin-bottom: 1em;
+        }
+        [data-dw-form].dw-is-loading {
+            min-height: 10em;
+            background-color: rgba(0,0,0,.05);
+        }
+        [data-dw-form].dw-is-loading::before {
+            box-sizing: border-box;
+            content: "";
+            position: absolute;
+            display: block;
+            font-size: 2em;
+            width: 1em;
+            height: 1em;
+            top: 50%;
+            left: 50%;
+            margin-top: -0.5em;
+            border: 5px solid #ddd;
+            border-top: 5px solid #222;
+            border-radius: 50%;
+            animation: dw-loading-spin 1s linear infinite;
+        }
+        @keyframes dw-loading-spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(359deg); }
+        }
+    `);
+
+    // Render all embedded forms
     const formEmbeds = document.querySelectorAll("[data-dw-form]");
     formEmbeds.forEach((embed) => {
         renderForm(embed);
@@ -24,7 +57,7 @@ async function renderForm(embed) {
     const PING_INTERVAL = embed.dataset.pingInterval;
 
     // Add loading state
-    embed.classList.add("is-loading");
+    embed.classList.add("dw-is-loading");
 
     try {
 
@@ -41,7 +74,7 @@ async function renderForm(embed) {
         await specification.render(embed);
 
         // Remove loading style
-        embed.classList.remove("is-loading");
+        embed.classList.remove("dw-is-loading");
 
         // (Optional) Prevent Specification timeout
         if (PING_INTERVAL) {
@@ -65,8 +98,14 @@ async function renderForm(embed) {
  */
 function pingSpecification(specification, interval) {
 
+    // Disable ping if interval is not a number
+    if (Number.isInteger(parseInt(interval)) === false) {
+        console.error("ERROR: Interval is not a valid integer (in whole seconds)");
+        return;
+    }
+
     // Disable ping if interval is 0
-    if (interval === 0 || Number.isInteger(parseInt(interval))) {
+    if (interval === 0) {
         return;
     }
 
@@ -74,18 +113,30 @@ function pingSpecification(specification, interval) {
     specification.ping();
 
     // Schedule next ping
-    setTimeout(pingSpecification, interval * 1000, specification);
+    setTimeout(function() {
+        pingSpecification(specification, interval);
+    }, interval * 1000);
 
 }
 
 /**
  * Strip trailing slash
  *
- * @param string String to remove trailing slash from
+ * @param {string} string String to remove trailing slash from
  */
 function stripTrailingSlash(string) {
     if (string.substr(-1) === "/") {
         return string.substr(0, string.length - 1);
     }
     return string;
+}
+
+/**
+ * Utility function to add CSS in multiple passes.
+ * @param {string} styleString
+ */
+function addHeaderStyle(styleString) {
+    const style = document.createElement("style");
+    style.textContent = styleString;
+    document.head.append(style);
 }
